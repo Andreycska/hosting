e2c9dfa39e13e3926e8f80be53ef83eb497792fc
Andreycska <pashenkoandrew@gmail.com>
2021-01-30 15:22:17 +0300

    modal

--- js/app.js
+++ js/app.js
@@ -64,3 +64,42 @@ let header = $("#header");
         arrows: false,
         dots: true
     });
+
+    // MODAL-BLOCK
+
+    const modalCall = $("[data-modal]"); //сохраняем все кнопки с атрибутом data-modal
+    const modalClose = $("[data-close]"); //сохраняем все кнопки close с атрибутом data-close
+
+    modalCall.on("click", function(event) { //Событие по клику на кнопку с атрибутом data-modal
+
+        event.preventDefault();
+        // let $this = $(this); //Сохраняем конкрутную кнопку
+        // let modalId = $this.data("modal"); //Сохраняем конкретное значение data-modal
+
+        $(".modal").addClass("show"); // показываем модальное окно
+        $("body").addClass("lock"); //убираем скролл у body
+    });
+
+    modalClose.on("click", function(event) { //Событие по клику на кнопку с атрибутом data-close
+
+        event.preventDefault();
+        let $this = $(this); //Сохраняем конкрутную кнопку close
+        let modalParent = $this.parents(".modal"); //Ищем родителя с классом modal
+
+        modalParent.removeClass("show"); // Убираем модальное окно
+        $("body").removeClass("lock"); //Добывляем скролл к body
+
+    });
+
+    $(".modal").on("click", function(event) { //Событие по клику на фон чтобы закрыть модальное окно
+
+        $(this).removeClass("show"); // Убираем модальное окно
+        $("body").removeClass("lock"); //Добывляем скролл к body
+
+    });
+
+    $(".modal__work").on("click", function(event) { //Событие по клику на само содержимое модального окна
+
+        event.stopPropagation(); //Отменяем событие клика по его радителю, тоесть на фоне закрывается окно, а на содержимом 
+
+    });
